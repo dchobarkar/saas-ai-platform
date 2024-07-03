7bdad54c1a67b671e31d0f66decf63694ea3b072
Darshan Chobarkar <dchobarkar@gmail.com>
2024-07-03 12:36:40 +0530

    add clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      Landing Page:
+      <Link href="sign-in">
+        <Button>Log in</Button>
+      </Link>
+      <Link href="sign-up">
+        <Button>Register</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default LandingPage;
