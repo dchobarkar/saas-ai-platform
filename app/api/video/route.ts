25c513518dd85389a8f63b0c9ce618e1d19bdc2a
Darshan Chobarkar <dchobarkar@gmail.com>
2024-07-11 13:00:46 +0530

    add video generation ai

---
+++ app/api/video/route.ts
@@ -0,0 +1,38 @@
+import Replicate from "replicate";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!prompt)
+      return new NextResponse("Prompt is required.", { status: 400 });
+
+    const input = {
+      fps: 24,
+      width: 1024,
+      height: 576,
+      prompt,
+      guidance_scale: 17.5,
+    };
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      { input }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
