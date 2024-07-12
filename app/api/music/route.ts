056cfc578ee1a0ed997d86cb4d5964b98991c775
Darshan Chobarkar <dchobarkar@gmail.com>
2024-07-12 13:14:28 +0530

    add api limit

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,6 +2,8 @@ import Replicate from "replicate";
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
@@ -17,6 +19,10 @@ export async function POST(req: Request) {
     if (!prompt)
       return new NextResponse("Prompt is required.", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+    if (!freeTrial)
+      return new NextResponse("Free trial has expired.", { status: 403 });
+
     const input = {
       prompt_b: prompt,
     };
@@ -24,6 +30,8 @@ export async function POST(req: Request) {
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       { input }
     );
+
+    await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,6 +2,8 @@ import Replicate from "replicate";
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
@@ -17,6 +19,10 @@ export async function POST(req: Request) {
     if (!prompt)
       return new NextResponse("Prompt is required.", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+    if (!freeTrial)
+      return new NextResponse("Free trial has expired.", { status: 403 });
+
     const input = {
       fps: 24,
       width: 1024,
@@ -29,6 +35,8 @@ export async function POST(req: Request) {
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       { input }
     );
+
+    await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
